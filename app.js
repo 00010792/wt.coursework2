81c8bd05e3733dc2b9e9065e3374c48580b521a3
00010792 <wiut00010792@gmail.com>
2021-04-07 18:37:56 +0500

    button added + router fixed

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const app = express();
-const indexRouter = require("./routes");
+const router = require("./routes");
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+app.use("/", router);
 // app.use("/api/v1/todo", apiRouter);
 
 // catch 404 and forward to error handler
